e4f0cc43b07e7832e1895970e9cb4f3013939a07
Rio Edwards <rioredwards@gmail.com>
2022-09-12 17:49:58 -0700

    Add Guess-State

--- app.js
+++ app.js
@@ -2,14 +2,26 @@
 import { getRandomItem } from './utils.js';
 
 /* State */
-
-/* Actions */
-
-/* Components */
+let gameState = 'guess'; // 'guess' or 'results'
 
 /* Component */
-// get DOM
+// Get DOM
+const results = document.getElementById('results');
+const playAgainBtn = document.getElementById('play-again-button');
+
 // display
+/* Actions */
+function displayShells(gameState) {
+    if (gameState === 'guess') {
+        results.classList.add('hidden');
+        playAgainBtn.classList.add('hidden');
+    }
+}
+
+function loadPage() {
+    displayShells(gameState);
+}
+
 // event listeners
 
 /* Run page load code */
